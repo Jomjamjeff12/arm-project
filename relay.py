23d17fb4812a3233bbd0f19c2207694201030a46
will presotn <williampreston2010@gmail.com>
2026-05-22 09:42:48 +0100

    testing transferring $scripts and $angle from reciever.sh to relay.py

---
+++ relay.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+source ~/arm-project/reciever.sh
+
+print(servo)
+print(angle)
--- send.sh
+++ send.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-read -p "servo num: " servo
-read -p "target angle: " angle
-
-echo "$servo" | ncat 100.118.65.57 5005
-echo "$angle" | ncat 100.118.65.57 5006
+echo "$1" | ncat 100.118.65.57 5005
+echo "$2" | ncat 100.118.65.57 5006
 
